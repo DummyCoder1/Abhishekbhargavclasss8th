4f6693d7c81c18ac940bfff470d84e79bdd497c8
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-03-04 12:29:55 +0530

    Add files via upload

---
+++ scratch_canvas (1).py
@@ -0,0 +1,12 @@
+a= int(input(''))
+def odd_even(a):
+    if a%2==0:
+        print('number is even no.')
+    else:
+        print('number is odd no.')
+
+odd_even(23)
+odd_even(25)
+odd_even(50)
+odd_even(70)
+odd_even(100)
